b8c73980645d99e30ec6a0cd73fc52aa5d901a6b
Joao Cury <verx.joao.cury@br.experian.com>
2019-05-31 18:18:19 -0300

    first commit, screens done

---
+++ app/src/main/java/com/curymorais/twitchtop100/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.curymorais.twitchtop100
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
